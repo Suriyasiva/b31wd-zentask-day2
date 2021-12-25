eea1354f7c784a6009e15073a7a5e566ef40134f
suriya <93177461+Suriyasiva@users.noreply.github.com>
2021-12-25 02:21:12 -0800

    Add files via upload

---
+++ day02.js
@@ -0,0 +1,54 @@
+// 1.difference between Browser JS(console) v Nodejs
+// Nodejs allows Javascript code to run outside the browser.
+// Nodejs comes with a lot of modules and mostly used in web development.
+// browers js(console)
+// Javascript is a programming language that is used for writing scripts on the website.
+// Javascript can only be run in the browsers.
+// It is basically used on the client-side.
+// Javascript is capable enough to add HTML and play with the DOM.
+// Javascript can run in any browser engine as like JS core in safari and Spidermonkey in Firefox. 
+// Javascript is used in frontend development.
+// Some of the javascript frameworks are nodejs, reactJS, etc
+// It is the upgraded version of ECMA script that uses Chrome’s V8 engine written in C++. 
+
+// node.js
+// NodeJS is a Javascript runtime environment.
+// We can run Javascript outside the browser with the help of NodeJS.
+// It is mostly used on the server-side
+// Nodejs does not have capability to add HTML tags
+// V8 is the Javascript engine inside of node.js that parses and runs Javascript. 
+// Nodejs is used in server-side development.
+// Some of the Nodejs modules are Lodash, express etc.
+
+// ---------------------------------------------------------------------------
+// 2.Execute the below code and write your description in txt file
+// typeof(1)=Number
+// typeof(1.1)=Number
+// typeof('1.1')=string
+// typeof(true)=boolean
+// typeof(null)=object
+// typeof(undefined)=undefined
+// typeof([])=object
+// typeof({})=object
+// typeof(NaN)=Number
+
+// ----------------------------------------------------------------------------
+// 3.Read what is prototype
+// Prototypes are the mechanism by which JavaScript objects inherit features from one another. 
+// This article covers traditional JavaScript constructors and classes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
